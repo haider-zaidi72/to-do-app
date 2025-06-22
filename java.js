2789abf0d660067b140ad3e0c4e5a8f9758b356a
haider-zaidi72 <suhz1100@gmail.com>
2025-06-22 16:41:31 +0500

    add

--- java.js
+++ java.js
@@ -1,35 +1,40 @@
+let data = JSON.parse(localStorage.getItem("tasks")) || [];
 
-let data = []
+window.onload = function () {
+    updateTaskList();
+};
 
 function addTask() {
+    let inPut = document.getElementById('todoapp');
+    let task = inPut.value.trim();
 
-let inPut = document.getElementById('todoapp');
-let task = inPut.value;
+    if (task === "") {
+        alert("Empty task is not allowed");
+        return;
+    }
 
-data.push(task);
-inPut.value = '';
-updateTaskList();
-
-if(data.value === ""){
-    alert("Empty task is not allowed");
-    return;
+    data.push(task);
+    inPut.value = '';
+    localStorage.setItem("tasks", JSON.stringify(data));
+    updateTaskList();
 }
-}
-function updateTaskList (){
+
+function updateTaskList() {
     let taskList = document.getElementById('taskList');
-    let taskCount = document.getElementById('taskcount')
+    let taskCount = document.getElementById('taskcount');
     taskList.innerHTML = '';
 
-    data.forEach((task, index)=>{
+    data.forEach((task, index) => {
         let li = document.createElement('li');
         li.textContent = task;
         taskList.appendChild(li);
-        
-    } )
+    });
+
     taskCount.textContent = data.length;
 }
 
-function clearAll(){
-    data.length = 0;
+function clearAll() {
+    data = [];
+    localStorage.removeItem("tasks");
     updateTaskList();
 }
